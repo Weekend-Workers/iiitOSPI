9d5f3e8b572c23b0a63e629cde4ab499dc987909
Kannav Mehta <kmkannavkmehta@gmail.com>
2021-06-19 21:39:19 +0530

    Minor Fixes, add login logout

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,6 +8,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
+        authenticated: false,
         accessToken: "", // github access token to acces the github api
         userProfile: {}  // user profile object
     },
@@ -17,19 +18,23 @@ export default new Vuex.Store({
         },
         setAccessToken(state, val) {
             state.accessToken = val;
+        },
+        setAuthenticated(state, val: boolean) {
+            state.authenticated = val;
         }
     },
     actions: {
         async login({ dispatch, commit }) {
-            auth.signInWithPopup(provider).then((result) => {
+            try {
+                const result = await auth.signInWithPopup(provider)
                 /** @type {firebase.auth.OAuthCredential} */
-                const credential = <firebase.auth.OAuthCredential> result.credential;
+                const credential = <firebase.auth.OAuthCredential>result.credential;
                 // This gives you a GitHub Access Token. You can use it to access the GitHub API.
                 const user = result.user;
                 const token = credential.accessToken;
                 dispatch('fetchUserProfile', user);
                 commit('setAccessToken', token);
-            }).catch((error) => {
+            } catch (error) {
                 // Handle Errors here.
                 const errorCode = error.code;
                 const errorMessage = error.message;
@@ -37,12 +42,28 @@ export default new Vuex.Store({
                 const email = error.email;
                 // // The firebase.auth.AuthCredential type that was used.
                 const credential = error.credential;
-            });
+            }
         },
         async fetchUserProfile({ commit }, user) {
             const userProfile = await usersCollection.doc(user.uid).get()
+            if (!userProfile.exists) {
+                usersCollection.doc(user.uid).set({
+                    username: user.displayName,
+                    email: user.email
+                })
+            }
             commit('setUserProfile', userProfile.data())
+            commit('setAuthenticated', true);
             router.push('/')
+        },
+        async signOut({ commit }) {
+            try {
+                const result = await auth.signOut()
+                commit('setAuthenticated', false);
+            } catch (error) {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            }
         }
     },
     modules: {},
